b2d89c42d239a7b265af552ef799a78fdce6e440
Nin3lee <2091392083@qq.com>
2023-09-18 12:00:36 +0800

    Site updated: 2023-09-18 12:00:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/14/hello-world/","2023/09/12/page/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/12/page/","2023/09/14/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
